b3c3e75f3fa7e6218bbd271ecd0656dd1cd040d8
alwayalive <alwayalive@163.com>
2016-05-24 12:29:42 +0800

    update date-helper to date-format

--- test/test.js
+++ test/test.js
@@ -1,17 +1,17 @@
-var dateHelper = require("../date-helper"),
+var dateFormat = require("../date-format"),
 	assert = require("assert");
 	
-describe("dateHelper test",function(){
+describe("dateFormat test",function(){
 
 	describe("date pattern transitor",function(){
 		
 		it("print default pattern : yyyy-MM-dd hh:mm:ss",function(){
-			var dataStr = dateHelper();
+			var dataStr = dateFormat();
 			assert.strictEqual( typeof dataStr.toString() , "string" );
 		});
 
 		it("print variety pattern",function(){
-			var dataStr = dateHelper("2011-01-10");
+			var dataStr = dateFormat("2011-01-10");
 			assert.strictEqual( dataStr.toString("yyyy年MM月dd日") , "2011年01月10日" );
 			assert.strictEqual( dataStr.toString("%y年%M月%d日") , "2011年01月10日" );
 			assert.strictEqual( dataStr.toString("MM/dd") , "01/10" );
@@ -23,28 +23,28 @@ describe("dateHelper test",function(){
 
 		//前几个月与后几个月
 		it("month",function(){
-			var dataStr = dateHelper("2011-05-10","yyyy-MM-dd");
+			var dataStr = dateFormat("2011-05-10","yyyy-MM-dd");
 			assert.strictEqual( dataStr.addMonth(1).toString() , "2011-06-10" );
 			assert.strictEqual( dataStr.addMonth(-1).toString() , "2011-05-10" );
 		});
 
 		//前几个天与后几个天
 		it("day",function(){
-			var dataStr = dateHelper("2011-05-10","yyyy-MM-dd");
+			var dataStr = dateFormat("2011-05-10","yyyy-MM-dd");
 			assert.strictEqual( dataStr.addDay(10).toString() , "2011-05-20" );
 			assert.strictEqual( dataStr.addDay(-10).toString() , "2011-05-10" );
 		});
 
 		//前几小时与后几小时
 		it("hour",function(){
-			var dataStr = dateHelper("2011-05-10 12:10:00");
+			var dataStr = dateFormat("2011-05-10 12:10:00");
 			assert.strictEqual( dataStr.addHour(10).toString() , "2011-05-10 22:10:00" );
 			assert.strictEqual( dataStr.addHour(-10).toString() , "2011-05-10 12:10:00" );
 		});
 
 		//前几分钟与后几分钟
 		it("minute",function(){
-			var dataStr = dateHelper("2011-05-10 12:10:00");
+			var dataStr = dateFormat("2011-05-10 12:10:00");
 			assert.strictEqual( dataStr.addMinu(10).toString() , "2011-05-10 12:20:00" );
 			assert.strictEqual( dataStr.addMinu(-10).toString() , "2011-05-10 12:10:00" );
 		});
